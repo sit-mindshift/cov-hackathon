3046a19845e5ce010969cacb09cc72474cd20f58
sebastian kroll <sebastian.kroll@lidl.com>
2020-03-20 21:15:28 +0100

    Sprinb boot app running

--- backend/src/main/java/com/sit/cov/hackatron/backend/BackendApplication.java
+++ backend/src/main/java/com/sit/cov/hackatron/backend/BackendApplication.java
@@ -1,7 +1,12 @@
 package com.sit.cov.hackatron.backend;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +14,18 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
 
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 }
