9019f308c456a7a2b0cea3128676ebaf6cfc9149
sebastian kroll <sebastian.kroll@lidl.com>
2020-03-20 21:15:28 +0100

    Sprinb boot app running

---
+++ backend/src/main/java/com/sit/cov/hackatron/backend/HelloController.java
@@ -0,0 +1,12 @@
+package com.sit.cov.hackatron.backend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
