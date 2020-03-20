4e26806180aea9a0278abbf1f0b104b41577e1bd
Manuel Münster <Manuel.Muenster@live.com>
2020-03-20 23:11:39 +0100

    Setup Storefinder

---
+++ backend/src/main/java/com/sit/cov/hackatron/backend/model/Coordinates.java
@@ -0,0 +1,15 @@
+package com.sit.cov.hackatron.backend.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Coordinates {
+    private String longitude;
+    private String latitude;
+}
