7f858d93f0f96bc43366530921c8443880441d79
Alex Rieger <mail@alex-rieger.de>
2020-03-21 03:11:18 +0100

    add login / register view

--- view/src/App.vue
+++ view/src/App.vue
@@ -2,21 +2,35 @@
   <div id="app">
       <navbar></navbar>
       <main id="app-main-content">
-        <router-view/>
+        <!-- normal router view for logged in users -->
+        <template v-if="userIsLoggedIn">
+          <router-view/>
+        </template>
+
+        <!-- login screen -->
+        <template v-else>
+          <Login/>
+        </template>
+
       </main>
   </div>
 </template>
 
-<script>
+<script lang="ts">
   import {Component, Vue} from 'vue-property-decorator';
-  import Navbar from './components/Navbar';
+  import Navbar from './components/Navbar.vue';
+  const Login = () => import('./views/Login.vue');
 
   @Component({
     components: {
       Navbar,
+      Login,
     },
   })
   export default class App extends Vue {
+    get userIsLoggedIn(): boolean {
+      return true
+    }
   }
 </script>
 
