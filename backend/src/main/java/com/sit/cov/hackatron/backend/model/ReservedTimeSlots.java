5f4e645dd0866faeb409404a6077a99defa5d510
Manuel Münster <Manuel.Muenster@live.com>
2020-03-21 16:54:46 +0100

    timeslot invalidate

--- backend/src/main/java/com/sit/cov/hackatron/backend/model/ReservedTimeSlots.java
+++ backend/src/main/java/com/sit/cov/hackatron/backend/model/ReservedTimeSlots.java
@@ -1,5 +1,6 @@
 package com.sit.cov.hackatron.backend.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
@@ -7,17 +8,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "timeslot")
 public class ReservedTimeSlots {
     @Id
     private String userId;
     private List<TimeSlot> timeSlots;
-    private List<TimeSlot> invalidTimeslSlots;
+    private List<TimeSlot> invalidTimeSlots = new ArrayList<>();
 
     public ReservedTimeSlots(String userId, List<TimeSlot> timeSlots) {
         this.userId = userId;
