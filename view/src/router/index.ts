3c939bbbabc78cd938ee7b28eead23ef5bcdbfd1
Manuel Münster <Manuel.Muenster@live.com>
2020-03-22 00:53:19 +0100

    Many squashed commits

--- view/src/router/index.ts
+++ view/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Detail from "@/views/Detail.vue";
 import QRCode from '../components/QRCode.vue'
 const Profile = () => import(/* webpackChunkName: "profile" */ '../views/Profile.vue');
 
@@ -18,6 +19,11 @@ const routes = [
     component: Profile,
   },
   {
+    path: '/shop/:id',
+    name: 'Detail',
+    component: Detail,
+  },
+  {
     path: '/codes',
     name: 'QRCodes',
     component: QRCode,
@@ -26,6 +32,7 @@ const routes = [
 
 const router = new VueRouter({
   routes,
+  mode: 'history',
 });
 
 export default router;
---
+++ view/src/views/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <b-form>
+      <b-container>
+        <ShopDetail/>
+      </b-container>
+    </b-form>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  import ShopDetail from "@/components/ShopDetail.vue";
+
+  @Component({
+    components: {
+      ShopDetail,
+    },
+  })
+  export default class Detail extends Vue {
+    // public selectedDate: string = "";
+  }
+</script>
