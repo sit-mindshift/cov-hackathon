c3569e036e94e77f53e9252a8ba186b9e24714f4
sebastian kroll <sebastian.kroll@lidl.com>
2020-03-21 11:46:55 +0100

    new Store service

--- backend/src/main/java/com/sit/cov/hackatron/backend/service/StoreService.java
+++ backend/src/main/java/com/sit/cov/hackatron/backend/service/StoreService.java
@@ -20,13 +20,30 @@ public class StoreService {
     private final WebClient webClient;
     //TODO Create configuration Files
     public static final String STORE_SEARCH_ENDPOINT = "https://mobile.lidl.de/Mobile-Server/service/1337/StoreSearch/nearestStore/DE/";
+    public static final String STORES_SEARCH_ENDPOINT = "https://mobile.lidl.de/Mobile-Server/service/1337/StoreSearch/DE/";
 
     public ResponseEntity<Store> getStores(String longitude, String latitude) {
-        return ResponseEntity.ok().body(webClient.get().uri(buildURI(longitude,latitude)).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(Store.class)
+        return ResponseEntity.ok().body(webClient.get().uri(buildStoresURI(longitude,latitude)).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(Store.class)
         .block());
     }
 
-    private String buildURI(String longitude, String latitude){
+    /**
+     * Example: https://mobile.lidl.de/Mobile-Server/service/1337/StoreSearch/DE/4912851/50000/925886/50000
+     * @param longitude the longitude. Example: 4912851
+     * @param latitude the latitude. Example: 925886
+     * @param span the size of the map you search on. Example 50000
+     * @return
+     */
+    public ResponseEntity<Store> getStores(String longitude, String latitude,String span) {
+        return ResponseEntity.ok().body(webClient.get().uri(buildStoresURI(longitude,latitude)).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(Store.class)
+                .block());
+    }
+
+    private String buildStoresURI(String longitude, String latitude){
         return STORE_SEARCH_ENDPOINT + "/" + longitude + "/" + latitude + "/";
+    }
+
+    private String buildStoresURI(String longitude, String latitude, String span){
+        return STORES_SEARCH_ENDPOINT + "/" + longitude + "/" + span + "/" + latitude + "/" + span;
     }
 }
