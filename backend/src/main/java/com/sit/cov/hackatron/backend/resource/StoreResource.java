4cef8b4c38371774f418766eb769af6ec64a3a90
Manuel Münster <Manuel.Muenster@live.com>
2020-03-21 02:05:35 +0100

    changed to query params

--- backend/src/main/java/com/sit/cov/hackatron/backend/resource/StoreResource.java
+++ backend/src/main/java/com/sit/cov/hackatron/backend/resource/StoreResource.java
@@ -2,12 +2,11 @@ package com.sit.cov.hackatron.backend.resource;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sit.cov.hackatron.backend.model.Coordinates;
 import com.sit.cov.hackatron.backend.service.StoreService;
 import com.sit.cov.hackatron.backend.model.Store;
 
@@ -26,7 +25,7 @@ public class StoreResource {
     //TODO Use nicer models, create real Reponse Object
     @GetMapping
     @ResponseBody
-    public ResponseEntity<Store> getStores(@RequestBody Coordinates coordinates) {
-        return storeService.getStores(coordinates.getLongitude(), coordinates.getLatitude());
+    public ResponseEntity<Store> getStores(@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude) {
+        return storeService.getStores(longitude, latitude);
     }
 }
