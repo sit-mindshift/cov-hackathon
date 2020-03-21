834c1606a7f9b2ec025b70570503b7a0b4f54782
Kevin Krauskopf <thosikrauskopf@web.de>
2020-03-21 23:08:21 +0100

    qr code view

--- view/src/store/models/user.ts
+++ view/src/store/models/user.ts
@@ -1,15 +1,17 @@
 import store, {RootState} from '../store';
 import {BareActionContext, getStoreBuilder} from 'vuex-typex';
-import shopRepository from '@/repositories/shopRepository';
+import qrcodeRepository from '@/repositories/qrcodeRepository';
 
 export interface UserState {
   longitude: string;
   latitude: string;
+  qrcodeData: any;
 }
 
 const initialUserState: UserState = {
   longitude: "4857204",
   latitude: "926664",
+  qrcodeData: null,
 };
 const b = getStoreBuilder<RootState>().module('user', initialUserState);
 
@@ -17,6 +19,17 @@ const b = getStoreBuilder<RootState>().module('user', initialUserState);
 function changePosition(state: UserState, payload: {longitude: string, latitude: string}) {
   state.longitude = payload.longitude;
   state.latitude = payload.latitude;
+}
+
+function setQRCodeData(state: UserState, payload: {data: any}) {
+  state.qrcodeData = payload.data;
+}
+
+// action
+async function readQRCodeData(context: BareActionContext<UserState, RootState>) {
+  const data: any = await qrcodeRepository.getQRCodes()
+    || initialUserState.qrcodeData;
+  user.setQRCodeData({data: data});
 }
 
 // state
@@ -31,6 +44,10 @@ const user = {
 
   // mutations
   changePosition: b.commit(changePosition),
+  setQRCodeData: b.commit(setQRCodeData),
+
+  //action
+  dispatchReadQRCodeData: b.dispatch(readQRCodeData),
 
 };
 
