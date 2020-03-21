2cfc8c8f7d8cd478daadbfee6d94a132f87566cb
Manuel Münster <Manuel.Muenster@live.com>
2020-03-21 11:31:53 +0100

    format

--- backend/src/main/java/com/sit/cov/hackatron/backend/service/QRCodeGeneratorService.java
+++ backend/src/main/java/com/sit/cov/hackatron/backend/service/QRCodeGeneratorService.java
@@ -11,22 +11,23 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class QRCodeGeneratorService {
-    
-        public Mono<byte[]> generateQRCode(String text, int width, int height) {
-    
-            Assert.hasText(text, "text must not be empty");
-            Assert.isTrue(width > 0, "width must be greater than zero");
-            Assert.isTrue(height > 0, "height must be greater than zero");
-            
-            return Mono.create(image -> {
-                try {
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
-                    BitMatrix matrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height);
-                    MatrixToImageWriter.writeToStream(matrix, MediaType.IMAGE_PNG.getSubtype(), baos, new MatrixToImageConfig());
-                    image.success(baos.toByteArray());
-                } catch (IOException | WriterException ex) {
-                    image.error(ex);
-                }
-            });
-        }    
+
+    public Mono<byte[]> generateQRCode(String text, int width, int height) {
+
+        Assert.hasText(text, "text must not be empty");
+        Assert.isTrue(width > 0, "width must be greater than zero");
+        Assert.isTrue(height > 0, "height must be greater than zero");
+
+        return Mono.create(image -> {
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                BitMatrix matrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height);
+                MatrixToImageWriter.writeToStream(matrix, MediaType.IMAGE_PNG.getSubtype(), baos,
+                        new MatrixToImageConfig());
+                image.success(baos.toByteArray());
+            } catch (IOException | WriterException ex) {
+                image.error(ex);
+            }
+        });
+    }
 }
