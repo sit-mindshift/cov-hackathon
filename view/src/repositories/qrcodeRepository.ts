370ce7053b038eedc1656a59bf412f23d5cd5533
calieston <calieston@hotmail.com>
2020-03-22 16:00:59 +0100

    fetch user id when reserving a timeslot, prepare fetch qr codes from user

--- view/src/repositories/qrcodeRepository.ts
+++ view/src/repositories/qrcodeRepository.ts
@@ -1,13 +1,17 @@
 /* tslint:disable:no-console */
 import httpClient from '@/utils/HttpClient';
 import user from "@/store/models/user";
+import timeslot from "@/store/models/timeslot";
 
 async function getQRCodes() {
   try {
+    let userId = user.state.personalData.id;
+    console.log('GET QR CODES FOR USER: '+ userId);
+
     const response =
       await httpClient.get('http://localhost:8080/qr-api/', {
         params: {
-          value: "hehexd"
+          value: userId
         }});
     return response;
   } catch (error) {
